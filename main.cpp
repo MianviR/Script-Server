828d3e17c6d6c51f90de3e2119aff848e912e454
mianvi <mianvimicro@outlook.com>
2024-11-05 20:50:07 +0300

    first local commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <boost/asio.hpp>
+#include <functional>
+#include <iostream>
+
+using namespace boost::asio;
+
+class printer
+{
+public:
+	printer(io_context& io) :
+		timer_(io, chrono::seconds(1)),
+		count_(0) 
+	{
+		timer_.async_wait(std::bind(&printer::print, this));
+	}
+	~printer() 
+	{
+		std::cout << "Final count is " << count_ << std::endl;
+	}
+
+	void print()
+	{
+		if (count_ < 5)
+		{
+			std::cout << count_ << std::endl;
+			++count_;
+		
+			timer_.expires_at(timer_.expiry() + chrono::seconds(1));
+			timer_.async_wait(std::bind(&printer::print, this)); 
+		} 
+	}
+
+private:
+	steady_timer timer_;
+	int count_;
+};
+
+int main() {
+	io_context io;
+	printer p(io);
+	io.run();
+	return 0;
+}
