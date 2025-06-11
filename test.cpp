983569ce273f9f030c13257e3d074fae3fe2961b
MianviR <gg.mancj@gmail.com>
2025-06-11 11:33:05 +0300

    deleted:    CMakeLists.txt 	deleted:    build/CMakeCache.txt 	deleted:    build/CMakeFiles/4.0.2-dirty/CMakeCCompiler.cmake 	deleted:    build/CMakeFiles/4.0.2-dirty/CMakeCXXCompiler.cmake 	deleted:    build/CMakeFiles/4.0.2-dirty/CMakeDetermineCompilerABI_C.bin 	deleted:    build/CMakeFiles/4.0.2-dirty/CMakeDetermineCompilerABI_CXX.bin 	deleted:    build/CMakeFiles/4.0.2-dirty/CMakeSystem.cmake 	deleted:    build/CMakeFiles/4.0.2-dirty/CompilerIdC/CMakeCCompilerId.c 	deleted:    build/CMakeFiles/4.0.2-dirty/CompilerIdC/a.out 	deleted:    build/CMakeFiles/4.0.2-dirty/CompilerIdCXX/CMakeCXXCompilerId.cpp 	deleted:    build/CMakeFiles/4.0.2-dirty/CompilerIdCXX/a.out 	deleted:    build/CMakeFiles/CMakeConfigureLog.yaml 	deleted:    build/CMakeFiles/CMakeDirectoryInformation.cmake 	deleted:    build/CMakeFiles/InstallScripts.json 	deleted:    build/CMakeFiles/Makefile.cmake 	deleted:    build/CMakeFiles/Makefile2 	deleted:    build/CMakeFiles/TargetDirectories.txt 	deleted:    build/CMakeFiles/client.dir/DependInfo.cmake 	deleted:    build/CMakeFiles/client.dir/build.make 	deleted:    build/CMakeFiles/client.dir/client.cpp.o 	deleted:    build/CMakeFiles/client.dir/client.cpp.o.d 	deleted:    build/CMakeFiles/client.dir/cmake_clean.cmake 	deleted:    build/CMakeFiles/client.dir/compiler_depend.internal 	deleted:    build/CMakeFiles/client.dir/compiler_depend.make 	deleted:    build/CMakeFiles/client.dir/compiler_depend.ts 	deleted:    build/CMakeFiles/client.dir/depend.make 	deleted:    build/CMakeFiles/client.dir/flags.make 	deleted:    build/CMakeFiles/client.dir/link.d 	deleted:    build/CMakeFiles/client.dir/link.txt 	deleted:    build/CMakeFiles/client.dir/progress.make 	deleted:    build/CMakeFiles/cmake.check_cache 	deleted:    build/CMakeFiles/progress.marks 	deleted:    build/CMakeFiles/server.dir/DependInfo.cmake 	deleted:    build/CMakeFiles/server.dir/build.make 	deleted:    build/CMakeFiles/server.dir/cmake_clean.cmake 	deleted:    build/CMakeFiles/server.dir/compiler_depend.internal 	deleted:    build/CMakeFiles/server.dir/compiler_depend.make 	deleted:    build/CMakeFiles/server.dir/compiler_depend.ts 	deleted:    build/CMakeFiles/server.dir/depend.make 	deleted:    build/CMakeFiles/server.dir/flags.make 	deleted:    build/CMakeFiles/server.dir/link.d 	deleted:    build/CMakeFiles/server.dir/link.txt 	deleted:    build/CMakeFiles/server.dir/progress.make 	deleted:    build/CMakeFiles/server.dir/server.cpp.o 	deleted:    build/CMakeFiles/server.dir/server.cpp.o.d 	deleted:    build/Makefile 	deleted:    build/client 	deleted:    build/cmake_install.cmake 	deleted:    build/server 	deleted:    client.cpp 	deleted:    message.h 	deleted:    my_connection.h 	deleted:    server.cpp 	deleted:    test.cpp 	deleted:    utils.h

--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdint>
-
-int main()
-{
-	std::uint8_t t = 122;
-	return 0;
-}
--- utils.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <boost/asio.hpp>
-#include <boost/array.hpp>
-#include <boost/bind/bind.hpp>
-#include <boost/enable_shared_from_this.hpp>
-
-using boost::asio::ip::tcp;
