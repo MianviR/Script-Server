d701e2a12d2827b6bf58fffada1eed7f6b52d142
MianviR <gg.mancj@gmail.com>
2025-06-23 22:37:58 +0300

    new file:   build/CMake 	modified:   build/CMakeCache.txt 	new file:   build/CMakeFiles/4.0.3-dirty/CMakeCCompiler.cmake 	new file:   build/CMakeFiles/4.0.3-dirty/CMakeCXXCompiler.cmake 	new file:   build/CMakeFiles/4.0.3-dirty/CMakeDetermineCompilerABI_C.bin 	new file:   build/CMakeFiles/4.0.3-dirty/CMakeDetermineCompilerABI_CXX.bin 	new file:   build/CMakeFiles/4.0.3-dirty/CMakeSystem.cmake 	new file:   build/CMakeFiles/4.0.3-dirty/CompilerIdC/CMakeCCompilerId.c 	new file:   build/CMakeFiles/4.0.3-dirty/CompilerIdC/a.out 	new file:   build/CMakeFiles/4.0.3-dirty/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   build/CMakeFiles/4.0.3-dirty/CompilerIdCXX/a.out 	modified:   build/CMakeFiles/CMakeConfigureLog.yaml 	modified:   build/CMakeFiles/Makefile.cmake 	modified:   build/CMakeFiles/Makefile2 	modified:   build/CMakeFiles/TargetDirectories.txt 	new file:   build/CMakeFiles/client.dir/compiler_depend.internal 	modified:   build/CMakeFiles/progress.marks 	new file:   build/CMakeFiles/server.dir/compiler_depend.internal 	new file:   build/CMakeFiles/server_beast.dir/DependInfo.cmake 	new file:   build/CMakeFiles/server_beast.dir/build.make 	new file:   build/CMakeFiles/server_beast.dir/cmake_clean.cmake 	new file:   build/CMakeFiles/server_beast.dir/compiler_depend.internal 	new file:   build/CMakeFiles/server_beast.dir/compiler_depend.make 	new file:   build/CMakeFiles/server_beast.dir/compiler_depend.ts 	new file:   build/CMakeFiles/server_beast.dir/depend.make 	new file:   build/CMakeFiles/server_beast.dir/flags.make 	new file:   build/CMakeFiles/server_beast.dir/link.d 	new file:   build/CMakeFiles/server_beast.dir/link.txt 	new file:   build/CMakeFiles/server_beast.dir/progress.make 	new file:   build/CMakeFiles/server_beast.dir/server_beast.cpp.o 	new file:   build/CMakeFiles/server_beast.dir/server_beast.cpp.o.d 	modified:   build/Makefile 	new file:   build/html/editScript.html 	new file:   build/html/newScript.html 	deleted:    build/server 	new file:   build/server_beast 	deleted:    src/client.cpp 	deleted:    src/message.h 	deleted:    src/my_connection.h 	deleted:    src/server.cpp 	deleted:    src/utils.h 	modified:   src/CMakeLists.txt 	modified:   src/server_beast.cpp

---
+++ build/CMakeFiles/server_beast.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server_beast.dir/link.d"
+  "CMakeFiles/server_beast.dir/server_beast.cpp.o"
+  "CMakeFiles/server_beast.dir/server_beast.cpp.o.d"
+  "server_beast"
+  "server_beast.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server_beast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/server_beast.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for server_beast.
+# This may be replaced when dependencies are built.
